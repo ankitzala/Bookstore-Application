7ae5bfdfb58c5a690ae5b8bd5c510708d2186404
Ankit zala <ankit851812@gmail.com>
2025-12-31 17:24:00 +0530

    Implemented model and controller to fetch and display data in browser

---
+++ Controllers/BookController.cs
@@ -0,0 +1,39 @@
+﻿using Bookstore_Application.Models;
+using Bookstore_Application.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bookstore_Application.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly BookRepositery _bookRepositery;
+        public BookController() {
+            _bookRepositery = new BookRepositery();
+        }
+
+        public List<BookModel> GetAllBooks() {
+
+            return _bookRepositery.GetAllBooks();
+
+            //return "All Boooks";
+            ////https://localhost:7174/book/GetAllBooks
+        }
+
+        public BookModel? GetBook(int id) {
+            return _bookRepositery.GetBookById(id);
+
+        //    return $"dotnet with id = {id}";
+        //https://localhost:7174/book/getbook/4
+
+        }
+
+        public List<BookModel> SearchBooks(string bookName ,string authorName)
+        {
+            return _bookRepositery.SesrchBook(bookName, authorName);
+
+            //return $"Book Name = {bookName} and Author is {authorName}";
+
+            //https://localhost:7174/book/SearchBooks?bookName=MVCbook&authorName=Ankit%20Zala
+        }
+    }
+}
---
+++ Repository/BookRepositery.cs
@@ -0,0 +1,39 @@
+﻿using Bookstore_Application.Models;
+using System.Linq;
+
+
+namespace Bookstore_Application.Repository
+{
+    public class BookRepositery
+    {
+        public List<BookModel> GetAllBooks()
+        {
+            return DataSource();
+        }
+
+        public BookModel? GetBookById(int id)
+        {
+            return DataSource().FirstOrDefault(x => x.Id == id);
+        }
+
+        public List<BookModel> SesrchBook(string title,string authorName)
+        {
+            return DataSource().Where(x=> x.Title.Contains(title) || x.Author.Contains(authorName)).ToList();
+        }
+
+        private List<BookModel> DataSource()
+        {
+            return new List<BookModel>()
+            {
+                new BookModel(){Id =1,Title="MVC Book",Author="Ankit"},
+                new BookModel(){Id =2,Title="Python",Author="Ankit"},
+                new BookModel(){Id =3,Title="Java",Author="Ankit"},
+                new BookModel(){Id =4,Title="Dot net",Author="Ankit"},
+                new BookModel(){Id =5,Title="Ai/ML",Author="Ankit"},
+                new BookModel(){Id =6,Title="PHP",Author="Ankit"},
+
+
+            };
+        }
+    }
+}
